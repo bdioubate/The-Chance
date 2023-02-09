f09b7a4d1101c0b767a218582a5bb0e41091e9fe
bangali11 <bangalidioubate@gmail.com>
2023-02-09 17:39:37 +0100

    Create Component Login

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
-<script setup>
+<script>
+import LoginVue from './components/login.vue';
+
+export default {
+    name: "App",
+    components: {
+        LoginVue
+    }
+}
 </script>
 
 <template>
         <h1>Welcome to Chance</h1>
+        <LoginVue />
 </template>
 
 <style scoped>
